ea7e475d0ab5ce01733203956496df42eadee5fb
Olga1302G <81965841+Olga1302G@users.noreply.github.com>
2021-06-08 14:10:11 +0800

    Update Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,14 +6,14 @@ import java.sql.SQLException;
 
 public class Util {
     //private static final String DB_DRIVER = "org.h2.Driver";
-    private static final String DB_URL = "jdbc:h2:~/default";
-    private static final String DB_USERNAME = "";
-    private static final String DB_PASSWORD = "";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/mysql";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "12345root-";
 
     public Connection getConnection() {
         Connection connection = null;
         try {
-            Class.forName("org.h2.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
             System.out.println("Connection OK");
         } catch (ClassNotFoundException | SQLException e) {
