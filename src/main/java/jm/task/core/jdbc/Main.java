382df728d482275a8fff6cf572f879219d0f2ef5
Olga1302G <81965841+Olga1302G@users.noreply.github.com>
2021-06-17 20:04:47 +0800

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -11,21 +11,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         UserService userService = new UserServiceImpl();
+            try {
             userService.createUsersTable();
-            userService.saveUser("Santa", "Klaus", (byte)98);
-        userService.saveUser("SantaOne", "Barbara", (byte)120);
-        userService.saveUser("Anna", "Red", (byte)38);
-        userService.saveUser("Serje", "Golon", (byte)24);
-        List <User> usersList = userService.getAllUsers();
-            for (User o: usersList) {
+
+            userService.saveUser("Santa", "Klaus", (byte) 98);
+            userService.saveUser("SantaOne", "Barbara", (byte) 120);
+            userService.saveUser("Anna", "Red", (byte) 38);
+            userService.saveUser("Serje", "Golon", (byte) 24);
+            List<User> usersList = userService.getAllUsers();
+            for (User o : usersList) {
                 System.out.println(o);
             }
             userService.removeUserById(1);
             userService.cleanUsersTable();
             userService.dropUsersTable();
+        } catch (SQLException e) {
 
+        }
         // реализуйте алгоритм здесь
     }
 }
