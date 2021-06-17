3172fb2bd9a0511a1eb5e6debe15125092679c29
Olga1302G <81965841+Olga1302G@users.noreply.github.com>
2021-06-17 18:16:23 +0800

    Update UserDaoJDBCImpl.java

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,6 +3,9 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
+import java.sql.*;
+import java.util.ArrayList;
+
 
 public class UserDaoJDBCImpl implements UserDao {
     private Connection connection = getConnection();
